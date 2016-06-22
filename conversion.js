591284495ba2fcd9880eb9ebacf0a2506cf17f46
Antonin Zoubian <antonin.zoubian@gmail.com>
2016-06-22 12:54:29 +0200

    modification de la fonction conversion adin de convertir les euros en francs et en livre anglaise

--- conversion.js
+++ conversion.js
@@ -1,6 +1,7 @@
 function conversion()
 {
    var somme = prompt("Entrez la valeur en Euros :");
-   var resultat = somme * 1,30;
-   alert(somme + " E\n" + resultat + "Livre Anglaise ");
+   var resultat1 = somme * 1,30;
+   var resultat2 = somme * 6,5;
+   alert(somme + " E\n" + resultat1 + "Livre Anglaise"+ resultat2 + "Francs");
 }
